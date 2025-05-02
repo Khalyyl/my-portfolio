0e805e15f737f6c2c0f1c3c2432019dd1ef9225a
Khalyyl <khalilaskri588@gmail.com>
2025-05-02 23:56:55 +0100

    make changes

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -11,18 +11,53 @@ import { ProjectsService } from '../_services/projects.service';
 })
 export class PortfolioComponent implements OnInit {
 
-  projects = {} as Project[];
+  projects = [] as Project[];
+  filteredProjects = [] as Project[];
+  selectedTags = [] as Tag[];
   // Une variable qui permettre de savoir si notre section de filtrage est reduite ou non
   isCollapsed: boolean = true;
 
+  allTags = [
+    Tag.ANGULAR,
+    Tag.TYPESCRIPT,
+    Tag.PYTHON,
+    Tag.JAVA,
+    Tag.JAVASCRIPT,
+    Tag.REACT,
+    Tag.TALEND,
+    Tag.POWERBI
+  ];
 
-    
-
-    constructor(private titleService: Title, private projectService : ProjectsService ){
+  constructor(private titleService: Title, private projectService : ProjectsService ){
     this.titleService.setTitle('Khalil Askri - Portfolio');
-    }
+  }
   ngOnInit(): void {
     this.projects = this.projectService.GetProjects();
+    this.filteredProjects = this.projects;
+  }
+
+  toggleTagSelection(tag: Tag): void {
+    const index = this.selectedTags.indexOf(tag);
+    if (index > -1) {
+      this.selectedTags.splice(index, 1);
+    } else {
+      this.selectedTags.push(tag);
+    }
+    this.filterProjects();
+  }
+
+  filterProjects(): void {
+    if (this.selectedTags.length === 0) {
+      this.filteredProjects = this.projects;
+    } else {
+      this.filteredProjects = this.projects.filter(project =>
+        project.tags.some(tag => this.selectedTags.includes(tag))
+      );
+    }
+  }
+
+  isTagSelected(tag: Tag): boolean {
+    return this.selectedTags.includes(tag);
   }
 
 
